e766164fa3976f55cfa0feb20c8ec52b408fdabd
takeshi-nomura <148610974+takeshi-nomura@users.noreply.github.com>
2023-12-11 00:25:25 +0900

    Add files via upload

--- single.php
+++ single.php
@@ -1,9 +1,3 @@
-<?php
-/*
-Template Name:イベント投稿ページ
-Template Post Type:post
-*/
-?>
 
 <?php get_header(); ?>
 <?php if (have_posts()) : ?>
@@ -46,9 +40,9 @@ Template Post Type:post
                         <div class="col-lg-2 col-md-2 col-sx-12">
                           <div class="calendar_news_border">
                             <div class="calendar_news_border_1">
-                              <div class="calendar_month"><?php echo post_custom('month'); ?></div>
+                              <div class="calendar_month"><?php echo get_post_time('F'); ?></div>
                               <div class="calendar_day">
-                              <span><?php echo post_custom('day'); ?></span><span>日</span>
+                              <span><?php echo get_the_date('d'); ?></span><span>日</span>
                               </div>
                             </div>
                           </div>
@@ -61,21 +55,7 @@ Template Post Type:post
                           <div class="news_detail_title">
                             <?php the_title(); ?>
                           </div>
-                          <div class="news_time">
-                            <div>
-                              <img src="<?php echo get_template_directory_uri(); ?>images/clock-regular.png" alt="" />
-                              <span>日付：<?php echo post_custom('date'); ?> </span>
-                            </div>
-                            <div>
-                              <img src="<?php echo get_template_directory_uri(); ?>images/yen-sign-solid.png" alt="" />
-                              <span>参加費：<?php
-                                        if (post_custom('fee') == 0) {
-                                          echo '無料';
-                                        } else {
-                                          echo '¥' . number_format(post_custom('fee'));
-                                        } ?></span>
-                            </div>
-                          </div>
+                     
                           <div class="news_post_meta">
                             <?php echo the_content(); ?>
                           </div>
